a79792ddc50a9fdd42b5b9f81a784a5d175a4001
nixxquality <nixx@is-fantabulo.us>
2020-11-19 19:55:46 +0100

    function pointer example

--- AoC2020/Warmup/2017_8.cpp
+++ AoC2020/Warmup/2017_8.cpp
@@ -9,16 +9,40 @@
 
 using namespace std;
 
+typedef bool(*comparator)(int, int);
+
+bool gt(int a, int b) {
+  return a > b;
+}
+
+bool ge(int a, int b) {
+  return a >= b;
+}
+
+bool lt(int a, int b) {
+  return a < b;
+}
+
+bool le(int a, int b) {
+  return a <= b;
+}
+
+bool ne(int a, int b) {
+  return a != b;
+}
+
+bool eq(int a, int b) {
+  return a == b;
+}
+
 int main() {
 
-    ifstream input("C:\\Users\\Martijn\\Desktop\\AoC2020\\AoC2020\\input\\day8_2017.txt");
+    ifstream input("input.txt");
 
     string x;
 
     //1: register, 2: dec/inc, 3: val, 4: cond_reg, 5: cond_cond, 6: cond_val 
     regex rule("(\\w+) (dec|inc) (-?[0-9]+) if (\\w+) (<|>|>=|<=|==|!=) (-?[0-9]+)");
-
-    enum comparators { gt, ge, lt, le, ne, eq };
 
     map<string, int> registers;
 
@@ -26,7 +50,7 @@ int main() {
     int inc[1000];
     int val[1000];
     string cond_reg[1000];
-    comparators cond_cond[1000];
+    comparator cond_cond[1000];
     int cond_val[1000];
 
     int index = 0;
@@ -35,7 +59,6 @@ int main() {
 
             smatch sm;
             regex_match(x, sm, rule);
-
 
             registers.insert(pair<string, int>(sm[1], 0));
 
@@ -72,28 +95,7 @@ int main() {
     int max_val_ever = 0;
     for (int i = 0; i < index; i++) {
         int cond_reg_value = registers[cond_reg[i]];
-        bool result = false;
-        switch (cond_cond[i]) {
-        case gt:
-            result = cond_reg_value > cond_val[i];
-            break;
-        case ge:
-            result = cond_reg_value >= cond_val[i];
-            break;
-        case lt:
-            result = cond_reg_value < cond_val[i];
-            break;
-        case le:
-            result = cond_reg_value <= cond_val[i];
-            break;
-        case ne:
-            result = cond_reg_value != cond_val[i];
-            break;
-        case eq:
-            result = cond_reg_value == cond_val[i];
-            break;
-        }
-        if (result) {
+        if (cond_cond[i](cond_reg_value, cond_val[i])) {
             registers[reg[i]] +=  inc[i] * val[i];
             if (registers[reg[i]] > max_val_ever)
                 max_val_ever = registers[reg[i]];
@@ -111,4 +113,3 @@ int main() {
     cout << max_val << endl;
     cout << max_val_ever << endl;
 }
-
